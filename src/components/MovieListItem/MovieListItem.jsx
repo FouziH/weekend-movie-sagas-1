271f8bbc5cce5b6426bfa77ebc70bc00ccb42c0b
Brad Johansen <brad.d.johansen@gmail.com>
2021-08-29 13:33:19 -0500

    done cleaning up component section

--- src/components/MovieListItem/MovieListItem.jsx
+++ src/components/MovieListItem/MovieListItem.jsx
@@ -1,13 +1,14 @@
 /**** SYSTEM ****/
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-/**** COMPONENTS ****/
 
 /**** STYLING ****/
 import { Grid, Typography, Paper } from '@material-ui/core';
 
 const MovieListItem = ({ movie }) => {
+
     const history = useHistory();
+
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Paper 
@@ -17,16 +18,23 @@ const MovieListItem = ({ movie }) => {
                 }}
                 elevation={12}
             >
-                <Typography noWrap component="h3" gutterBottom>{movie.title}</Typography>
+                <Typography 
+                    noWrap 
+                    component="h3" 
+                    gutterBottom
+                >
+                    {movie.title}
+                </Typography>
                 <img 
                     src={movie.poster} 
                     alt={movie.title}
                     onClick={ () => history.push(`/details/${movie.id}`)}
                     style={{ height: 250,}}
-                /><br/>
+                />
             </Paper>
         </Grid>
-    )
-}
+    ) // End MovieListItem return
+
+} // End MovieListItem()
 
 export default MovieListItem;
